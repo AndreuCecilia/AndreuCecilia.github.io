2c114d4f463c5918373d82bf65d06be47a74c971
Ariadna <Andreu.cecilia@upc.edu>
2024-05-28 17:43:24 +0200

    Guardar cambios antes del pull

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config'
-
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
 
-    site: 'https://AndreuCecilia.github.io',
-    base: 'WebAndreuCecilia',
 
 });
 
